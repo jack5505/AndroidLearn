e92c8ffc13f9b3cf439d06b11a1238a088fdb159
Jakhongir Sabirov <jahon.080879@gmail.com>
2019-09-30 09:42:04 +0500

    Adding from one intent to other

--- app/src/main/java/com/example/jsabirov/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/jsabirov/myapplication/Main2Activity.java
@@ -1,11 +1,13 @@
 package com.example.jsabirov.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -15,6 +17,10 @@ public class Main2Activity extends AppCompatActivity {
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(intent.getStringExtra("first")+" "+intent.getStringExtra("second"));
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,4 +32,27 @@ public class Main2Activity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("BEFORE START second ACTIVITY");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("PAUSE second ACTIVITY");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("STOP second ACTIVIY");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("Destroyed second Activity");
+    }
 }
---
+++ app/src/main/java/com/example/jsabirov/myapplication/Main3Activity.java
@@ -0,0 +1,44 @@
+package com.example.jsabirov.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Main3Activity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editText,editText1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        editText = findViewById(R.id.login);
+        editText1 = findViewById(R.id.password);
+        Button btn = findViewById(R.id.ok);
+        Button bt = findViewById(R.id.cancel);
+        bt.setOnClickListener(this);
+        btn.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.cancel:
+            editText.setText("");
+            editText1.setText("");
+            break;
+            case R.id.ok:
+                Intent intent = new Intent(this,Main2Activity.class);
+                intent.putExtra("first",editText.getText().toString());
+                intent.putExtra("second",editText1.getText().toString());
+                startActivity(intent);
+        }
+    }
+}
